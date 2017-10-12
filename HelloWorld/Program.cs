3035c3f9f6e0cf18811c434563f15e66b3dc25ce
Jaradan Murcott <jaradan.murcott@education.nsw.gov.au>
2017-10-12 12:39:34 +1100

    Made Hello world and welcome message

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            luc
-        }
+            Console.WriteLine("Hello World.");
+            Console.WriteLine("Hello User.");
+            Console.WriteLine("Earrape Engaged");
+            Console.Beep(10000, 1000000);
+            Console.Title = "Earrape Machine";
+            Console.ReadLine();
+        }   
     }
 }
---
+++ WelcomeMessage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            Console.Write("Enter Your Age: ");
+            string name = Console.ReadLine();
+            string age = Console.ReadLine();
+            Console.WriteLine("Welcome to my program " + name + ", your recorded age is " + age);
+
+
+            // Wait at the end
+            Console.ReadKey();
+        }
+    }
+}
