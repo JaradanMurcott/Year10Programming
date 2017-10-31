36197ef5fefb77c4829050ef8a6851a17a1d98b6
Jaradan Murcott <jaradan.murcott@education.nsw.gov.au>
2017-11-01 09:39:21 +1100

    FizzBuzz is complete

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10000000; i++) 
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Beep(700, 50);
+                }
+                else if(i%3==0)
+                {
+                    Console.WriteLine("Fizz");
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Beep(600, 50);
+
+                }
+                else if(i%5==0)
+                {
+                    Console.WriteLine("Buzz");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Beep(500, 50);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
