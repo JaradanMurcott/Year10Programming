ae40c89171a2c928f88c94631e23b3de347e3640
Jaradan Murcott <jaradan.murcott@education.nsw.gov.au>
2017-11-22 10:27:44 +1100

    Changes comitted

---
+++ Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Klaas", "Bootsma", 35);
+        }
+    }
+
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private bool sitting;
+        private int steps;
+
+        public Person(string firstName, string lastName, int age);
+        
+        
+    }
+}
