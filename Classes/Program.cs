75918222918627bfa11fcf77683b2a1b0923f118
Jaradan Murcott <jaradan.murcott@education.nsw.gov.au>
2017-11-23 12:41:46 +1100

    Classes almost finished

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -11,6 +11,8 @@ namespace Classes
         static void Main(string[] args)
         {
             Person p = new Person("Klaas", "Bootsma", 35);
+            p.Sit();
+            p.Stand();
         }
     }
 
@@ -22,8 +24,47 @@ namespace Classes
         private bool sitting;
         private int steps;
 
-        public Person(string firstName, string lastName, int age);
-        
-        
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            sitting = true;
+            steps = 0;
+        }
+        public void PrintInfo()
+        {
+            if (sitting)
+            {
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, is currently sitting" + $"and has taken {steps} steps");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, is currently standing" +
+                    $"          " + $"and has taken {steps} steps");
+            }
+        }
+        public void Sit()
+        {
+            if (sitting)
+            {
+                Console.WriteLine($"{firstName} {lastName} is already sitting");
+            }
+            else
+            {
+                sitting = true;
+            }
+        }
+        public void Stand()
+        {
+            if (sitting)
+            {
+                sitting = false;
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} is already standing");
+            }
+        }
     }
 }
